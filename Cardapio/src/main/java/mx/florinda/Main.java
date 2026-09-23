0396927f25c34298aac885123567ebbca5ec2a62
Luis <martinsluisdev@gmail.com>
2026-09-23 09:53:06 -0300

    feat(Main): cria instancia de ItemCardapio, converte para json usando o Gson e imprime resultado

--- Cardapio/src/main/java/mx/florinda/Main.java
+++ Cardapio/src/main/java/mx/florinda/Main.java
@@ -1,7 +1,19 @@
 package mx.florinda;
 
+import com.google.gson.Gson;
+
+import java.math.BigDecimal;
+import static mx.florinda.ItemCardapio.CategoriaCardapio.BEBIDAS;
+
 public class Main {
     static void main() {
-        IO.println("Hello, World!");
+        ItemCardapio refrescoDoChaves = new ItemCardapio(1l, "Refresco do Chaves", """
+                Suco de limão, que parece tamarindo ma tem gosto de groselha
+                """, BEBIDAS, new BigDecimal("2.99"), null);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(refrescoDoChaves);
+
+        System.out.println(json);
     }
 }
