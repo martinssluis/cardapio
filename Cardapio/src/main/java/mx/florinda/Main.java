9989928c11f1ce70bab13f8d0ac36cacd6b8eca9
Luis <martinsluisdev@gmail.com>
2026-09-25 15:54:34 -0300

    feat(Main): cria instancia de HistoricoVisualizacao, registra visualizações, mostra taltal de visualizados e imprime a lista de visualizações

--- Cardapio/src/main/java/mx/florinda/Main.java
+++ Cardapio/src/main/java/mx/florinda/Main.java
@@ -1,22 +1,16 @@
 package mx.florinda;
 
-import java.util.*;
-
-import static mx.florinda.ItemCardapio.CategoriaCardapio.*;
-
 public class Main {
     static void main() {
         Database database = new Database();
-        List<ItemCardapio> itens = database.listaDeItensCardapio();
-        itens.forEach(System.out::println);
 
-        Set<ItemCardapio.CategoriaCardapio> categoriasCardapio = EnumSet.of(SOBREMESA, ENTRADA);
-        categoriasCardapio.forEach(System.out::println);
+        HistoricoVisualizacao historico = new HistoricoVisualizacao(database);
+        historico.registrarVisualizacao(1l);
+        historico.registrarVisualizacao(2l);
+        historico.registrarVisualizacao(4l);
+        historico.registrarVisualizacao(6l);
 
-        // Preciso das descrições associadasas categorias em promocao
-        Map<ItemCardapio.CategoriaCardapio, String> promocoes = new EnumMap<>(ItemCardapio.CategoriaCardapio.class);
-        promocoes.put(SOBREMESA, "O doce perfeito para você");
-        promocoes.put(ENTRADA, "come sua refeição com um toque de sabor");
-        System.out.println(promocoes);
+        historico.mostrarTotalVitualizados();
+        historico.listaVisualizacoes();
     }
 }
