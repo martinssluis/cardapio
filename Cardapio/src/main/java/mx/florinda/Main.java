d30a601cbac271beec01a703065f94dfaa0b1115
Luis <martinsluisdev@gmail.com>
2026-09-25 14:25:34 -0300

    feat(Main): cria um EnumMap para guardar as Categorias em promoção e uma descrição

--- Cardapio/src/main/java/mx/florinda/Main.java
+++ Cardapio/src/main/java/mx/florinda/Main.java
@@ -13,6 +13,10 @@ public class Main {
         Set<ItemCardapio.CategoriaCardapio> categoriasCardapio = EnumSet.of(SOBREMESA, ENTRADA);
         categoriasCardapio.forEach(System.out::println);
 
-
+        // Preciso das descrições associadasas categorias em promocao
+        Map<ItemCardapio.CategoriaCardapio, String> promocoes = new EnumMap<>(ItemCardapio.CategoriaCardapio.class);
+        promocoes.put(SOBREMESA, "O doce perfeito para você");
+        promocoes.put(ENTRADA, "come sua refeição com um toque de sabor");
+        System.out.println(promocoes);
     }
 }
