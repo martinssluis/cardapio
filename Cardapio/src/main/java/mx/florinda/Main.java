b1bac3f6c636ee148d18fe87884a60c838b607fa
Luis <martinsluisdev@gmail.com>
2026-09-25 14:20:31 -0300

    feat(Principal): usa EnumSet para manter as categorias que estão em promoção

--- Cardapio/src/main/java/mx/florinda/Main.java
+++ Cardapio/src/main/java/mx/florinda/Main.java
@@ -10,24 +10,9 @@ public class Main {
         List<ItemCardapio> itens = database.listaDeItensCardapio();
         itens.forEach(System.out::println);
 
-        System.out.println("--------");
+        Set<ItemCardapio.CategoriaCardapio> categoriasCardapio = EnumSet.of(SOBREMESA, ENTRADA);
+        categoriasCardapio.forEach(System.out::println);
 
-        Optional<ItemCardapio> optionalItem = database.itemCardapioPorId(6L);
-        String mensagem = optionalItem.
-                map(ItemCardapio::toString)
-                .orElse("Não encontrado");
-        System.out.println(mensagem);
 
-        System.out.println("--------");
-        //Precisa manter as categorias que estão em promocao
-        Set<ItemCardapio.CategoriaCardapio> categoriasPromocao = new TreeSet<>();
-        categoriasPromocao.add(SOBREMESA);
-        categoriasPromocao.add(ItemCardapio.CategoriaCardapio.ENTRADA);
-        categoriasPromocao.forEach(System.out::println);
-
-        System.out.println("--------");
-
-        Set<ItemCardapio.CategoriaCardapio> categoriaCardapio2 = Set.of(SOBREMESA, ENTRADA);
-        categoriaCardapio2.forEach(System.out::println);
     }
 }
