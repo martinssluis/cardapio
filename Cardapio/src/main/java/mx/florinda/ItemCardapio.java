48e6001e9c1daa7dcc70e24f6417e0e05d1c002b
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-25 20:25:38 -0300

    feat(ItemCardapio): cria metodo alterarPreco que retorna uma nova instancia de ItemCardapio, já que o Record é imutável

--- Cardapio/src/main/java/mx/florinda/ItemCardapio.java
+++ Cardapio/src/main/java/mx/florinda/ItemCardapio.java
@@ -5,6 +5,10 @@ import java.math.BigDecimal;
 public record ItemCardapio(Long id, String nome, String descricao, CategoriaCardapio categoria, BigDecimal preco,
                            BigDecimal precoComDesconto) {
 
+    public ItemCardapio alterarPreco(BigDecimal novoPreco) {
+        return new ItemCardapio(id, nome, descricao, categoria, novoPreco, precoComDesconto);
+    }
+
     public enum CategoriaCardapio {
         ENTRADA, PRATOS_PRINCIPAIS, BEBIDAS, SOBREMESA
     }
