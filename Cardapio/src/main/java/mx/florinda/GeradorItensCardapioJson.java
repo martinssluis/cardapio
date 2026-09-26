743034f663e864be02c40a9a16e29b0ae67be517
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-26 11:53:52 -0300

    feat(GeradorItensCardapioJson): salva json em um arquivo

--- Cardapio/src/main/java/mx/florinda/GeradorItensCardapioJson.java
+++ Cardapio/src/main/java/mx/florinda/GeradorItensCardapioJson.java
@@ -2,17 +2,22 @@ package mx.florinda;
 
 import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class GeradorItensCardapioJson {
-    static void main() {
+    static void main() throws IOException {
         Database database = new Database();
         List<ItemCardapio> listaItensCardapio = database.listaDeItensCardapio();
 
         Gson gson = new Gson();
         String json = gson.toJson(listaItensCardapio);
 
-        System.out.println(json);
+        Path path = Path.of("itensCardapio.json");
+        Files.writeString(path, json);
+
     }
 
 }
