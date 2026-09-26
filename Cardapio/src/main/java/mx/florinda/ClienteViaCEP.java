4c8147bffade245512708e602115a04f4df97397
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-26 11:39:30 -0300

    feat(ClienteViaCEP): faz conexão com ViaCEP por meio do HttpClient

--- Cardapio/src/main/java/mx/florinda/ClienteViaCEP.java
+++ Cardapio/src/main/java/mx/florinda/ClienteViaCEP.java
@@ -1,18 +1,27 @@
 package mx.florinda;
 
-import java.net.URL;
-import java.util.Scanner;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ClienteViaCEP {
     static void main() throws Exception {
 
-        URL url = new URL("https://viacep.com.br/ws/01001000/json/");
+        //URL url = new URL("https://viacep.com.br/ws/01001000/json/");
 
-        try (Scanner scanner = new Scanner(url.openStream())){
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
-                System.out.println(line);
-            }
+        URI uri = URI.create("https://viacep.com.br/ws/01001000/json/");
+
+        try(HttpClient httpClient = HttpClient.newHttpClient()){
+            HttpRequest httpRequest = HttpRequest.newBuilder(uri).build();
+           HttpResponse<String> httpResponse = httpClient.send(httpRequest,
+                    HttpResponse.BodyHandlers.ofString());
+
+            int satatusCode = httpResponse.statusCode();
+            String body = httpResponse.body();
+            System.out.println(satatusCode);
+            System.out.println(body);
         }
+
     }
 }
